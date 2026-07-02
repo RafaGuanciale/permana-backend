95a9b1735502e62d72b7828b0d559e0e2d10e19a
Rafael <rafaelguanciale@hotmail.com>
2026-07-02 11:45:25 -0300

    Refactor: Port removida do app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('./config/db');
 const cors = require('cors');
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT } = process.env;
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const errorHandler = require('./middleware/errorHandler');
